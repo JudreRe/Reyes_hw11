da7c58ccc3c2329abde05c412162a0419ee80ad7
Judre <Reyesjudre@gmail.com>
2020-11-23 17:29:29 -0800

    WiP

--- index.js
+++ index.js
@@ -56,42 +56,6 @@ app.get("/books", (req, res) => {
   });
 });
 
-const sql_create = `CREATE TABLE IF NOT EXISTS Books (
-  Book_ID SERIAL PRIMARY KEY,
-  Title VARCHAR(100) NOT NULL,
-  Author VARCHAR(100) NOT NULL,
-  Comments TEXT
-);`;
-
-pool.query(sql_create, [], (err, result) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log("Successful creation of the 'Books' table");
-});
-
-console.log("Successful creation of the 'Books' table");
-
-
-// Database seeding
-const sql_insert = `INSERT INTO Books (Book_ID, Title, Author, Comments) VALUES
-  (1, 'Mrs. Bridge', 'Evan S. Connell', 'First in the serie'),
-  (2, 'Mr. Bridge', 'Evan S. Connell', 'Second in the serie'),
-  (3, 'L''ingénue libertine', 'Colette', 'Minne + Les égarements de Minne')
-ON CONFLICT DO NOTHING;`;
-pool.query(sql_insert, [], (err, result) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  const sql_sequence = "SELECT SETVAL('Books_Book_ID_Seq', MAX(Book_ID)) FROM Books;";
-  pool.query(sql_sequence, [], (err, result) => {
-    if (err) {
-      return console.error(err.message);
-    }
-    console.log("Successful creation of 3 books");
-  });
-});
-
 app.get("/books", (req, res) => {
   const sql = "SELECT * FROM Books ORDER BY Title"
   pool.query(sql, [], (err, result) => {
